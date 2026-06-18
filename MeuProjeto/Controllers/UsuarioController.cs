9cb572b7c9e35d8effe76ddbaeef3f0acabeff0e
Lemoel Mendes <225293226+lemoelmendes@users.noreply.github.com>
2026-06-18 13:54:58 -0300

    feat: adicionar criacao e navegacao de usuarios para administradores

--- MeuProjeto/Controllers/UsuarioController.cs
+++ MeuProjeto/Controllers/UsuarioController.cs
@@ -35,5 +35,51 @@ namespace MeuProjeto.Controllers
 
             return View(usuarios);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(UsuarioCreateViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            bool emailExiste = _context.Usuarios
+                .Any(u => u.Email == model.Email);
+
+            if (emailExiste)
+            {
+                ModelState.AddModelError(
+                    "",
+                    "Já existe um usuário com este e-mail.");
+
+                return View(model);
+            }
+
+            string hash =
+                BCrypt.Net.BCrypt.HashPassword(
+                    model.Senha);
+
+            var usuario = new Models.Usuario
+            {
+                Nome = model.Nome,
+                Email = model.Email,
+                SenhaHash = hash,
+
+                PerfilId = 2,
+
+                Ativo = true
+            };
+
+            _context.Usuarios.Add(usuario);
+
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
