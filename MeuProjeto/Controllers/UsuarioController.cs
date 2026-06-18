e61d60985b6eaaab0c3468f04c26537ca34b6d82
Lemoel Mendes <225293226+lemoelmendes@users.noreply.github.com>
2026-06-18 13:25:24 -0300

    feat: adicionar listagem de usuarios para administradores

---
+++ MeuProjeto/Controllers/UsuarioController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MeuProjeto.Data;
+using MeuProjeto.Filters;
+using MeuProjeto.ViewModels;
+
+namespace MeuProjeto.Controllers
+{
+    [Autenticado]
+    [SomenteAdmin]
+    public class UsuariosController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public UsuariosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var usuarios = _context.Usuarios
+                .Include(u => u.Perfil)
+                .Select(u => new UsuarioViewModel
+                {
+                    Id = u.Id,
+                    Nome = u.Nome,
+                    Email = u.Email,
+                    Perfil = u.Perfil != null
+                        ? u.Perfil.Nome
+                        : "",
+                    Ativo = u.Ativo
+                })
+                .ToList();
+
+            return View(usuarios);
+        }
+    }
+}
